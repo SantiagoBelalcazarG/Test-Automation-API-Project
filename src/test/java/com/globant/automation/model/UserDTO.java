4f11e65addf0b619c5dff54a09287d635f53866a
SantiagoBelalcazarG <santiago.belalcazar@globant.com>
2026-03-16 12:27:25 -0500

    refractor: rename CreateUserDTO to UserDTO

--- src/test/java/com/globant/automation/model/UserDTO.java
+++ src/test/java/com/globant/automation/model/UserDTO.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class CreateUserDTO {
+public class UserDTO {
 
     private Long id;
     private String username;
--- src/test/java/com/globant/automation/test/PostUserTest.java
+++ src/test/java/com/globant/automation/test/PostUserTest.java
@@ -2,7 +2,7 @@ package com.globant.automation.test;
 
 import com.globant.automation.config.TestRunner;
 import com.globant.automation.model.ApiResponseDTO;
-import com.globant.automation.model.CreateUserDTO;
+import com.globant.automation.model.UserDTO;
 import com.globant.automation.request.RequestBuilder;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
@@ -14,7 +14,7 @@ public class PostUserTest extends TestRunner {
 
     @Test(testName = "Validate user creation")
     public void createUserTest(){
-        CreateUserDTO createUserDTO = CreateUserDTO.builder()
+        UserDTO userDTO = UserDTO.builder()
                 .id(1L)
                 .username("santiagojb")
                 .password("1234test")
@@ -23,14 +23,14 @@ public class PostUserTest extends TestRunner {
                 .email("santiago@test.com")
                 .build();
 
-        Response response = RequestBuilder.postResquest(getBaseurl(), "/user", createUserDTO, getApikey());
+        Response response = RequestBuilder.postResquest(getBaseurl(), "/user", userDTO, getApikey());
         ApiResponseDTO apiResponseDTO = response.as(ApiResponseDTO.class);
 
         assertEquals(response.getStatusCode(), 200, "The status code doesn't match.");
         assertNotNull(apiResponseDTO, "The response body should not be null.");
         assertEquals(apiResponseDTO.getCode(), Integer.valueOf(200), "The response code should match.");
         assertEquals(apiResponseDTO.getType(), "unknown", "The response type should match.");
-        assertEquals(apiResponseDTO.getMessage(), String.valueOf(createUserDTO.getId()), "The message should match the user id.");
+        assertEquals(apiResponseDTO.getMessage(), String.valueOf(userDTO.getId()), "The message should match the user id.");
     }
 
 }
